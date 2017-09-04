26f2b61329d9cfc92f5ba920065d1e73f1ab3add
Carlos Beltran <carlos@doejo.com>
2017-09-04 18:31:34 -0500

    complete js handlers

--- src/js/app.js
+++ src/js/app.js
@@ -22,19 +22,34 @@ App = {
 
     return App.initWeb3();
   },
-
+  
   initWeb3: function() {
-    /*
-     * Replace me...
-     */
+    // metamask and mist inject their own web3 instances, so just 
+    // set the provider if it exists
+    if (typeof web3 !== "undefined") {
+      App.web3Provider = web3.currentProvider;
+      web3 = new Web3(web3.currentProvider);
+    } else {
+      // set the provider you want from Web3.providers
+      App.web3Provider = new web3.providers.HttpProvider("http://localhost:8545");
+      web3 = new Web3(App.web3Provider);
+    }
 
     return App.initContract();
   },
 
   initContract: function() {
-    /*
-     * Replace me...
-     */
+    $.getJSON('Adoption.json', function(data) {
+      // Get the necessary contract artifact file and instantiate it with truffle-contract.
+      var AdoptionArtifact = data;
+      App.contracts.Adoption = TruffleContract(AdoptionArtifact);
+
+      // Set the provider for our contract.
+      App.contracts.Adoption.setProvider(App.web3Provider);
+
+      // Use our contract to retieve and mark the adopted pets.
+      return App.markAdopted();
+    });
 
     return App.bindEvents();
   },
@@ -48,15 +63,43 @@ App = {
 
     var petId = parseInt($(event.target).data('id'));
 
-    /*
-     * Replace me...
-     */
+    var adoptionInstance;
+
+    web3.eth.getAccounts(function(error, accounts) {
+      if (error) {
+        console.log(error);
+      }
+
+      var account = accounts[0];
+
+      App.contracts.Adoption.deployed().then(function(instance) {
+        adoptionInstance = instance;
+
+        return adoptionInstance.adopt(petId, {from: account});
+      }).then(function(result) {
+        return App.markAdopted();
+      }).catch(function(err) {
+        console.log(err.message);
+      });
+    });
   },
 
   markAdopted: function(adopters, account) {
-    /*
-     * Replace me...
-     */
+    var adoptionInstance;
+
+    App.contracts.Adoption.deployed().then(function(instance) {
+      adoptionInstance = instance;
+
+      return adoptionInstance.getAdopters.call();
+    }).then(function(adopters) {
+      for (i = 0; i < adopters.length; i++) {
+        if (adopters[i] !== '0x0000000000000000000000000000000000000000') {
+          $('.panel-pet').eq(i).find('button').text('Pending...').attr('disabled', true);
+        }
+      }
+    }).catch(function(err) {
+      console.log(err.message);
+    });
   }
 
 };
